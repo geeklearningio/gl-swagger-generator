d8db6278e52b573f67447cfeaa871e5606988ca5
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-08-03 17:32:32 +0200

    push current progress

--- src/node-swagger-generator/src/sink.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by autex on 5/20/2016.
- */
-
-export interface ISink {
-
-}
---
+++ src/node-swagger-generator/tests/types/csharp-types.spec.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by autex on 5/31/2016.
+ */
+"use strict"
+
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
