ca388721ffcc1f1ba569049d50fa0c6ff3ac587a
Adrien Siffermann <adrien@siffermann.me>
2017-08-10 18:20:13 +0200

    Extend Angular 1.5 package mode with options

--- lib/languages/typescript.d.ts
+++
@@ -1,9 +0,0 @@
-import { ILanguageFilter, IGenerationContext } from '../generation';
-import { IType, IAbstractedTypeConverter } from '../typing';
-export declare function create(): ILanguageFilter;
-export declare class TypescriptFilter implements ILanguageFilter {
-    genericRegex: RegExp;
-    constructor();
-    createAbstractedTypeConverter(generationContext: IGenerationContext): IAbstractedTypeConverter<IType>;
-    supportsGenerics(): boolean;
-}
